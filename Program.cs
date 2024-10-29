8fba78ceae16d0a01e175bea7b629776fe129c97
KaoruH <tatiane_tanizaki@hotmail.com>
2024-10-29 14:50:06 -0300

    Iniciando Projeto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (minimal_api.Dominio.DTOs.LoginDTO loginDTO) =>
+{
+
+if (loginDTO.Email == "adm@teste.com" && loginDTO.Password == "123456")
+{
+    return Results.Ok("Login feito com sucesso!");
+}
+else
+{
+    return Results.Unauthorized();
+}
+
+});
+
+app.Run();
